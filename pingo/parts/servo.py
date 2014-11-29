b2d3b907e9e639228055e9518ca5704b7901977a
afonso <afonso@yack.com.br>
2014-11-29 15:50:29 -0200

    testando servo

--- pingo/parts/servo.py
+++ pingo/parts/servo.py
@@ -9,9 +9,8 @@ class Servo(object):
 		self.pin = pin
 
 	def degree(self, value):
-		micro_seconds = (value * (2350 - 500) / 180 + 500) / 10000000.0
+		micro_seconds = (value * (2350 - 500) / 180 + 500) / 1000000.0
 		self.pin.high()
 		time.sleep(micro_seconds)
 		self.pin.low()
-		time.sleep(0.4 - micro_seconds)
-		time.sleep(0.2 / 1000.0)
+		time.sleep(micro_seconds)
